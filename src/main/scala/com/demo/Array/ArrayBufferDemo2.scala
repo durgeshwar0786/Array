aa19af86868a091f8823657d5b6dc006c3f58367
Durgeshwar Mishra <corp.dpmishra@gmail.com>
2024-02-26 13:53:09 +0530

    Second Commit in Array Branch

--- src/main/scala/com/demo/Array/ArrayBufferDemo2.scala
+++ src/main/scala/com/demo/Array/ArrayBufferDemo2.scala
@@ -1,5 +1,20 @@
 package com.demo.Array
+import scala.collection.mutable.ArrayBuffer
+object ArrayBufferDemo2 extends App {
+  var arrBuffer = ArrayBuffer[Integer]()
 
-object ArrayBufferDemo2 {
+  arrBuffer += 9
+  arrBuffer += 7
+  arrBuffer += 3
+  arrBuffer += 6
+  arrBuffer += 5
+  var i = 0
+  for (i <- arrBuffer) {
+    println("before adding :" + i)
+  }
+  arrBuffer += 20
+  for (j <- arrBuffer) {
+    println(" After adding new elements:" + j)
+  }
 
 }
